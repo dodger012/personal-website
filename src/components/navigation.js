808ddb5283398ea6b0722ac89c5f2ec7e274185f
Matt Straat <matt_straathof@intuit.com>
2020-08-26 11:21:48 -0700

    fix: navigation links

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,10 +6,10 @@ export default () => (
   <nav role="navigation">
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
+        <Link to="/personal-website/">Home</Link>
       </li>
       <li className={styles.navigationItem}>
-        <Link to="/blog/">Blog</Link>
+        <Link to="/personal-website/blog/">Blog</Link>
       </li>
     </ul>
   </nav>
